833e6c29e238b87de8e0d8f9d8cd2ee219e79766
Abubaker <abubaker.surangiwala@icloud.com>
2025-01-30 17:59:21 -0600

    updating the index.html file, building the landing page, adding logo, adding folders for file structur

--- frontend_gostem/src/App.js
+++ frontend_gostem/src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import goStemLogo from './components/GoStemLogo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Landing-page">
+        <img src={goStemLogo} className="App-logo" alt="logo"/>
+        <button className='Login-button'> Login </button>
       </header>
     </div>
   );
