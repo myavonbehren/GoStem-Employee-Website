0338788e1401b47e96b9ca0afa7e1a9fc41cc896
Aadil Patel <aadilpatel6103@gmail.com>
2025-02-08 22:43:10 -0600

    Switch database configuration from PostgreSQL to SQLite in settings.py for local development

--- backend/gostem/settings.py
+++ backend/gostem/settings.py
@@ -37,11 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    'daphne',
+    #'daphne',
     "django.contrib.staticfiles",
     'polls.apps.PollsConfig',
     'rest_framework',
-    'channels',
+    #'channels',
     'notes',
 ]
 
@@ -85,23 +85,23 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gostem_db',  # Your PostgreSQL database name
-        'USER': 'gostem_user',  # Your PostgreSQL username
-        'PASSWORD': 'gostem',  # Your PostgreSQL password
-        'HOST': 'localhost',  # Change to your DB server if remote
-        'PORT': '5432',  # Default PostgreSQL port
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'gostem_db',  # Your PostgreSQL database name
+#         'USER': 'gostem_user',  # Your PostgreSQL username
+#         'PASSWORD': 'gostem',  # Your PostgreSQL password
+#         'HOST': 'localhost',  # Change to your DB server if remote
+#         'PORT': '5432',  # Default PostgreSQL port
+#     }
+# }
 
 
 
