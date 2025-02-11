aef469d23ac262356f94ebae20e2a011986c66ef
Dallard02 <dantheguitarman02@gmail.com>
2025-02-11 14:25:52 -0600

    Reapply "created notes app"
    This reverts commit 98831816310f0620653381728a23d4de0e1414a1.

--- backend/notes/serializer.py
+++ backend/notes/serializer.py
@@ -11,4 +11,9 @@
 # ctrl+shift+p, python: select interpeter, select global
 
 from rest_framework import serializers
+from .models import Note
 
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ['id', 'title', 'description', 'created_at', 'updated_at']
